78365699c4216ae47c63dc52d6cc43519357ab0d
erbhuvnesh <52860707+erbhuvnesh@users.noreply.github.com>
2025-02-19 01:46:00 +0530

    upload exam functionality added

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {getAvailableExams, getExamQuestions}  = require("../controllers/examController");
+const {getAvailableExams, getExamQuestions, uploadExam}  = require("../controllers/examController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get('/', authMiddleware(['Student','Admin']), getAvailableExams);
 router.get('/:exam_id', authMiddleware(['Student','Admin']), getExamQuestions);
+// POST /upload-exam (Admin only)
+router.post('/upload-exam', authMiddleware(['Admin']), uploadExam);
 
 module.exports = router;
