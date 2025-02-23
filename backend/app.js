d7ff68041f7c544f5a1266a946a2e8a9070d9303
abhay <abhayrajsinghrana123@gmail.com>
2025-02-23 17:27:00 +0530

    Revert "Reapply "Reapply "added result download page"""
    This reverts commit e49b768d2ae5cce5a0653263a276aabd3a1360be.

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,6 @@ app.use("/exams", examRoutes);
 app.use("/auth", authRoutes);
 app.use("/results", resultRoutes);
 app.use("/admin/exams", adminRoutes);
-app.use("/admin", adminRoutes);
 
 
 // dummy data
